64873a6de56434496b46cb7fd85e13012bb6e5e4
ebmarque <ebenezermarquezine@gmail.com>
2025-06-08 12:17:50 +0100

    fix: correct delete route in friendship management to include missing slash

--- src/routes/friendshipRoutes.ts
+++ src/routes/friendshipRoutes.ts
@@ -6,5 +6,5 @@ export default async function friendshipRoutes(app: FastifyInstance) {
   app.get('/requests/:userId', friendshipController.getFriendRequests); // ver pedidos recebidos
   app.patch('/respond/:friendshipId', friendshipController.respondToFriendRequest); // aceitar/rejeitar
   app.get('/list/:userId', friendshipController.listFriends); // listar amigos
-  app.delete('', friendshipController.removeFriend); // remover amizade
+  app.delete('/', friendshipController.removeFriend); // remover amizade
 }
